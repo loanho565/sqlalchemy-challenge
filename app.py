49bb38234b7f1bf81d0ed84551dafb41015dcc2e
Loan <loanho237@gmail.com>
2024-01-04 21:00:35 -0800

    modified app.py

--- app.py
+++ app.py
@@ -1,30 +1,148 @@
 # Import the dependencies.
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
+import datetime as dt
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+# Define a function which calculates and returns the the date one year from the most recent date
+def date_prev_year():
+    # Create the session
+    session = Session(engine)
+
+    # Define the most recent date in the Measurement dataset
+    # Then use the most recent date to calculate the date one year from the last date
+    most_recent_date = session.query(func.max(Measurement.date)).first()[0]
+    first_date = dt.datetime.strptime(most_recent_date, "%Y-%m-%d") - dt.timedelta(days=365)
+
+    # Close the session                   
+    session.close()
+
+    # Return the date
+    return(first_date)
+#################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+#################################################
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create the session
+    session = Session(engine)
+
+    # Query precipitation data from last 12 months from the most recent date from Measurement table
+    prcp_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= date_prev_year()).all()
+    
+    # Close the session                   
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of prcp_list
+    prcp_list = []
+    for date, prcp in prcp_data:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        prcp_list.append(prcp_dict)
+
+    # Return a list of jsonified precipitation data for the previous 12 months 
+    return jsonify(prcp_list)
+#################################################
+@app.route("/api/v1.0/stations")
+def stations():
+    """ Return a JSON list of stations from the dataset."""
+    # Query station data from the Station dataset
+    results = session.query(Station.station).all()
+    # Convert list of tuples into normal list
+    station_list = list(np.ravel(results))
+    return jsonify(station_list)
+#################################################
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Query tobs data from last 12 months from the most recent date from Measurement table
+    tobs_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281').\
+                        filter(Measurement.date >= date_prev_year()).all()
+
+    # Create a dictionary from the row data and append to a list of tobs_list
+    tobs_list = []
+    for date, tobs in tobs_data:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        tobs_list.append(tobs_dict)
+
+    # Return a list of jsonified tobs data for the previous 12 months
+    return jsonify(tobs_list)
+
+#################################################
+@app.route("/api/v1.0/<start>")
+def cal_temp(start):
+ # go back one year from start date and go to end of data for Min/Avg/Max temp   
+    most_recent_date = session.query(func.max(Measurement.date)).first()[0]
+    start_date= dt.datetime.strptime(most_recent_date, "%Y-%m-%d")
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end =  dt.date(2017, 8, 23)
+    trip_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+    
+@app.route("/api/v1.0/<start>/<end>")
+def cal_temp2(start,end):
+
+  # go back one year from start/end date and get Min/Avg/Max temp     
+    most_recent_date = session.query(func.max(Measurement.date)).first()[0]
+    least_recent_date = session.query(func.min(Measurement.date)).first()[0]
+    start_date= dt.datetime.strptime(most_recent_date, "%Y-%m-%d")
+    end_date= dt.datetime.strptime(least_recent_date,'%Y-%m-%d')
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end = end_date-last_year
+    trip_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+
+#################################################
+if __name__ == "__main__":
+    app.run(debug=True)
